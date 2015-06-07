9913004167fc74e340e819571893c421b809571e
Sahil Gupta <emailsahilgupta12@gmail.com>
2015-06-07 19:44:29 -0400

    Initial Commit

---
+++ Notebook/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Notebook
+//
+//  Created by Sahil Gupta on 2015-06-07.
+//  Copyright (c) 2015 SahilGupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Notebook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Notebook
+//
+//  Created by Sahil Gupta on 2015-06-07.
+//  Copyright (c) 2015 SahilGupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
